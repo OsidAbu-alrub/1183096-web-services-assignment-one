8e11d524a516b9c350569b89e781bcd079a18ada
Osid Abu-Alrub <o.Abu-Alrub@foothillsolutions.com>
2022-04-25 01:51:13 +0300

    add prefix to all URLs and configure prisma service

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import helmet from "helmet"
+import { PrismaService } from "./prisma/prisma.service"
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000
   console.log(`Application started on http://localhost:${PORT}/`)
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix("api/v1")
   app.use(helmet())
+  const prismaService = app.get(PrismaService)
+  await prismaService.enableShutdownHooks(app)
   await app.listen(PORT)
 }
 bootstrap()
