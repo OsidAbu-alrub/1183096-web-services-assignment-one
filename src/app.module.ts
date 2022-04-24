da6d8f386cf29184473d23369a4bf97b62a6c59b
Osid Abu-Alrub <o.Abu-Alrub@foothillsolutions.com>
2022-04-25 01:50:46 +0300

    expose prisma service and product module endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
-import { AppController } from "./app.controller"
-import { AppService } from "./app.service"
+import { ProductModule } from "./products/product.module"
+import { PrismaModule } from "src/prisma/prisma.module"
 
+// this is the entry point
+// if you dont import modules here, you will not be able
+// to use them in your application
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), PrismaModule, ProductModule],
 })
 export class AppModule {}
