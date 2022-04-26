edf4c808d1e3a001e67e8ec1667b8f2f8eda047f
Osid Abu-Alrub <o.Abu-Alrub@foothillsolutions.com>
2022-04-26 16:36:57 +0300

    add CategoryModule and remove circular dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
-import { ProductModule } from "./products/product.module"
+import { ProductModule } from "./product/product.module"
 import { PrismaModule } from "src/prisma/prisma.module"
+import { CategoryModule } from "./category/category.module"
 
 // this is the entry point
 // if you dont import modules here, you will not be able
 // to use them in your application
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    ProductModule,
+    CategoryModule,
+  ],
 })
 export class AppModule {}
