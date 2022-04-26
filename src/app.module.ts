10cadf6a65c739082f84b7e0faee42a9bb61a88b
Osid Abu-Alrub <o.Abu-Alrub@foothillsolutions.com>
2022-04-26 18:16:31 +0300

    add resources to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config"
 import { ProductModule } from "./product/product.module"
 import { PrismaModule } from "src/prisma/prisma.module"
 import { CategoryModule } from "./category/category.module"
+import { ProductCategoryModule } from "./product-category/product-category.module"
 
 // this is the entry point
 // if you dont import modules here, you will not be able
@@ -13,6 +14,7 @@ import { CategoryModule } from "./category/category.module"
     PrismaModule,
     ProductModule,
     CategoryModule,
+    ProductCategoryModule,
   ],
 })
 export class AppModule {}
